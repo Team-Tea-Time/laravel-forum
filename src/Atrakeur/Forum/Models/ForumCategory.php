6b38c3bffaf8e3cb17d6c281f22a480679d18e5a
atrakeur <atrakeur@gmail.com>
2014-05-20 19:42:58 +0200

    Refactored tabs

--- src/Atrakeur/Forum/Models/ForumCategory.php
+++ src/Atrakeur/Forum/Models/ForumCategory.php
@@ -8,27 +8,27 @@ class ForumCategory extends AbstractForumBaseModel {
 	protected $appends = array('topicCount', 'replyCount', 'lastReplyId');
 
 	public function parentCategory()
-    {
+	{
 		return $this->hasOne('\Atrakeur\Forum\Models\ForumCategory', 'parent_category');
 	}
 
 	public function subcategories()
-    {
+	{
 		return $this->hasMany('\Atrakeur\Forum\Models\ForumCategory', 'parent_category');
 	}
 
 	public function topics()
-    {
+	{
 		return $this->belongsTo('\Atrakeur\Forum\Models\ForumTopic');
 	}
 
 	public function scopeWhereTopLevel($query)
-    {
+	{
 		return $query->where('parent_category', '=', NULL);
 	}
 
 	public function getTopicCountAttribute()
-    {
+	{
 		$topicCount = $this->rememberAttribute('topicCount', function() {
 			echo 'load';
 			return $this->topics()->count();
@@ -37,7 +37,7 @@ class ForumCategory extends AbstractForumBaseModel {
 	}
 
 	public function getReplyCountAttribute()
-    {
+	{
 		$replyCount = $this->rememberAttribute('replyCount', function() {
 			$replyCount = 0;
 			$topics = $this->topics()->with('messages')->get();
@@ -50,7 +50,7 @@ class ForumCategory extends AbstractForumBaseModel {
 	}
 
 	public function getLastReplyIdAttribute()
-    {
+	{
 		//TODO
 	}
 
