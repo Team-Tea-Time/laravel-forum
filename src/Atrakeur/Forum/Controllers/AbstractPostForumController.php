24094f44c579ab17b83f6cf2fad7bc2c4b910334
atrakeur <atrakeur@gmail.com>
2014-05-29 13:33:28 +0200

    Added post action

--- src/Atrakeur/Forum/Controllers/AbstractPostForumController.php
+++ src/Atrakeur/Forum/Controllers/AbstractPostForumController.php
@@ -6,8 +6,29 @@ use \Atrakeur\Forum\Models\ForumMessage;
 
 abstract class AbstractPostForumController extends AbstractForumController {
 
+	protected $topicRules = array(
+			'title' => 'required',
+	);
+
+	protected $messageRules = array(
+			'data' => 'required|min:5',
+	);
+
+	protected function getCurrentUser() 
+	{
+		$userfunc = \Config::get('forum::integration.currentuser');
+		$user = $userfunc();
+		return $user;
+	}
+
 	public function getNewTopic($categoryId, $categoryUrl)
 	{
+		$user = $this->getCurrentUser();
+		if ($user == NULL) 
+		{
+			return \App::abort(403, 'Access denied');
+		}
+
 		$category       = ForumCategory::findOrFail($categoryId);
 		$category->load('parentCategory');
 		$parentCategory = $category->parentCategory;
@@ -18,7 +39,38 @@ abstract class AbstractPostForumController extends AbstractForumController {
 
 	public function postNewTopic($categoryId, $categoryUrl)
 	{
-		
+		$user = $this->getCurrentUser();
+		if ($user == NULL) 
+		{
+			return \App::abort(403, 'Access denied');
+		}
+
+		$category  = ForumCategory::findOrFail($categoryId);
+		$validator = \Validator::make(\Input::all(), array_merge($this->topicRules, $this->messageRules));
+		if ($validator->passes())
+		{
+			$title = \Input::get('title');
+			$data  = \Input::get('data');
+
+			$topic                  = new ForumTopic();
+			$topic->parent_category = $category->id;
+			$topic->author          = $user->id;
+			$topic->title           = $title;
+			$topic->save();
+
+			$message               = new ForumMessage();
+			$message->parent_topic = $topic->id;
+			$message->author       = $user->id;
+			$message->data         = $data;
+			$message->save();
+
+			return \Redirect::to($topic->url)
+					->with('success', 'topic created');
+		}
+		else {
+			return \Redirect::to($subCategory->url)
+					->withErrors($validator);
+		}
 	}
 
 }
