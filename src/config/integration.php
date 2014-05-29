4b38738ded394a322d2de61890217f290404716c
atrakeur <atrakeur@gmail.com>
2014-05-29 11:49:04 +0200

    Added user support

--- src/config/integration.php
+++ src/config/integration.php
@@ -14,6 +14,21 @@ return array(
 
 	/*
 	|--------------------------------------------------------------------------
+	| Closure to determine the current user model
+	|--------------------------------------------------------------------------
+	|
+	| Must return the current logged in user model to use
+	| Non object, or NULL response is considered as not logged in
+	|
+	*/
+	'currentuser' => function() {
+		//Here you can use confide facade, 
+		//or just the default facade, or whatever else
+		return Auth::user();
+	},
+
+	/*
+	|--------------------------------------------------------------------------
 	| Application forum controller
 	|--------------------------------------------------------------------------
 	|
