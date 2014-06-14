f389db5db28c7b1526cf927ef3fb4fba502f3ed5
atrakeur <atrakeur@gmail.com>
2014-06-14 12:07:09 +0200

    Added access control on postbutton

--- src/views/partials/postbutton.blade.php
+++ src/views/partials/postbutton.blade.php
@@ -1,7 +1,14 @@
 <?php
+/*
+ * The button should be displayed only when:
+ * 		The user is connected (not null)
+ * 		if an access model if given, the canPost($user) method return true
+ *
+ * Otherwise, the button shouldn't be displayed
+ */
 $userfunc = \Config::get('forum::integration.currentuser');
 $user = $userfunc();
 ?>
-@if ($user != null)
+@if ($user != null && (!isset($accessModel) || $accessModel->canPost($user) == true))
 	<a href="{{ $url }}" class="btn btn-primary">{{ $message }}</a>
 @endif
