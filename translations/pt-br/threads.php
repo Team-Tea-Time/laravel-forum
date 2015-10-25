3fd012f15791537c8566727d50766f14c30a63f4
Mateus Guimarães <mateus.jatene@gmail.com>
2015-10-24 21:31:21 -0300

    threads.php progress

---
+++ translations/pt-br/threads.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'actions' => "Ações do tópico",
+    'created' => "Tópico criado",
+    'delete' => "Deletar esse tópico",
+    'deleted' => "Tópico deletado|Tópicos deleted",
+    'lock' => "Lock",
+    'locked' => "Locked",
+    'marked_read' => "All Tópicos have been marked as read",
+    'new_thread' => "New thread",
+    'new_updated' => "New & updated Tópicos",
+    'newest' => "Newest thread",
+    'none_found' => "No Tópicos found",
+    'perma_deleted' => "Thread permanently deleted|Tópicos permanently deleted",
+    'pin' => "Pin",
+    'pinned' => "Pinned",
+    'post_the_first' => "Post the first!",
+    'restored' => "Thread restored|Tópicos restored",
+    'thread' => "Thread|Tópicos",
+    'updated' => "Thread updated|Tópicos updated",
+    'unlock' => "Unlock",
+    'unpin' => "Unpin",
+    'view' => "View thread",
+
+];
