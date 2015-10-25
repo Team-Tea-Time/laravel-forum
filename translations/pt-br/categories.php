5dadd6ee1397a4aaa6b97a809ee3fef445bbb0a5
Ricardo Barantini <ricardobarantini@gmail.com>
2015-10-24 22:16:32 -0200

    Translate the project to `pt-br`

---
+++ translations/pt-br/categories.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'actions'               => "Ações de categoria",
+    'category'              => "Categoria|Categorias",
+    'create'                => "Criar categoria",
+    'created'               => "Categoria criada",
+    'deleted'               => "Categoria deletada|Categorias deletadas",
+    'disable_threads'       => "Desabilitar tópicos",
+    'enable_threads'        => "Habilitar tópicos",
+    'make_private'          => "Tornar privado",
+    'make_public'           => "Tornar público",
+    'mark_read'             => "Marcar como lido os tópicos dessa categoria",
+    'marked_read'           => "Tópico novo/atualizado em :category foi marcado como lido",
+    'restored'              => "Categoria restaurada|Categorias restauradas",
+    'subcategories'         => "Subcategorias",
+    'threads_disabled'      => "Criar novo tópico está desativado nessa categoria",
+    'updated'               => "Categoria atualizada|Categorias atualizadas",
+
+];
