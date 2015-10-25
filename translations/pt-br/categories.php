2d01cda90453c2e1c282f0851525e6cfce465dea
Mateus Guimarães <mateus.jatene@gmail.com>
2015-10-24 21:21:15 -0300

    first pt-br translation commit

---
+++ translations/pt-br/categories.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'actions'               => "Category actions",
+    'category'              => "Category|Categories",
+    'create'                => "Create category",
+    'created'               => "Category created",
+    'deleted'               => "Category deleted|Categories deleted",
+    'disable_threads'       => "Disable threads",
+    'enable_threads'        => "Enable threads",
+    'make_private'          => "Make private",
+    'make_public'           => "Make public",
+    'mark_read'             => "Mark threads in this category as read",
+    'marked_read'           => "New/updated threads in :category have been marked as read",
+    'restored'              => "Category restored|Categories restored",
+    'subcategories'         => "Subcategories",
+    'threads_disabled'      => "New thread creation is disabled in this category",
+    'updated'               => "Category updated|Categories updated",
+
+];
