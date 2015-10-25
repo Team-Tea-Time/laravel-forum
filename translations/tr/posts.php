c015c8c1c208de2eed094a279bf621604bfe988a
Fatih Kahveci <f.kahveci@ymail.com>
2015-10-25 11:42:40 +0200

    Turkish translate

---
+++ translations/tr/posts.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'actions'       => "Mesaj ayarları",
+    'created'       => "Mesaj gönderildi",
+    'deleted'       => "Mesaj silindi|Mesajlar silindi",
+    'edit'          => "Mesaj düzenle",
+    'last'          => "Son mesaj",
+    'perma_deleted' => "Mesaj kalıcı silindi|Mesajlar kalıncı olarak silindi",
+    'post'          => "Mesaj|Mesajlar",
+    'restored'      => "Mesaj geri alındı|Mesajlar geri alındı",
+    'updated'       => "Mesaj güncellendi|Mesajlar güncellendi",
+    'view'          => "Mesajı görüntüle",
+    'your_post'     => "Sizin mesajınız",
+
+];
