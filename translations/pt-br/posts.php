e719d1454fd886fd7e0ebeb82bd08b042cd645c7
Mateus Guimarães <mateus.jatene@gmail.com>
2015-10-24 21:30:08 -0300

    posts.php

---
+++ translations/pt-br/posts.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'actions' => "Ações do post",
+    'created' => "Post criado",
+    'deleted' => "Post deletado|Posts deletados",
+    'edit' => "Editar post",
+    'last' => "Último post",
+    'perma_deleted' => "Post permanentemente deletado|Posts permanentemente deletados",
+    'post' => "Post|Posts",
+    'restored' => "Post restaurado|Posts restaurados",
+    'updated' => "Post atualizado|Posts atualizados",
+    'view' => "Ver post",
+    'your_post' => "Seu post",
+
+];
