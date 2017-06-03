a1f2e3dac765c720e502813cd77113217ef5a592
Şakir Mehmetoğlu (SuStartX) <sakir.mehmetoglu@gmail.com>
2017-06-03 12:00:45 +0300

    Update categories.php
    Syntax error fix

--- translations/tr/categories.php
+++ translations/tr/categories.php
@@ -11,11 +11,11 @@ return [
     'enable_threads'        => "Konuları aktif et",
     'make_private'          => "Özel olarak işaretle",
     'make_public'           => "Herkese açık olarak işaretle",
-    'mark_read'             => "Bu konuda ki konuları okundu olarak işaretle",
-    'marked_read'           => "Yeni/güncellenen şu :category deki konuları okundu olarak işaretlendi",
+    'mark_read'             => "Bu kategorideki konuları okundu olarak işaretle",
+    'marked_read'           => "Yeni/Güncellenen şu :category deki konuları okundu olarak işaretlendi",
     'restored'              => "Kategori geri alındı|Kategoriler geri alındı",
     'subcategories'         => "Alt Kategoriler",
-    'threads_disabled'      => "Bu kategori kiltili olduğundan buraya konu açamazsınız.",
+    'threads_disabled'      => "Bu kategori kilitli olduğundan buraya konu açamazsınız.",
     'updated'               => "Kategori güncellendi|Kategoriler güncelendi",
 
 ];
