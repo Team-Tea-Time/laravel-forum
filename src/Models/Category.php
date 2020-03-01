6505fe9de6f5962de46a764cb6762f0bb552b207
Rick Mann <3583774+Riari@users.noreply.github.com>
2020-03-01 22:14:07 +0000

    Update Category model

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -1,5 +1,8 @@
 <?php namespace TeamTeaTime\Forum\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Gate;
 use Kalnoy\Nestedset\NodeTrait;
 use TeamTeaTime\Forum\Support\Traits\CachesData;
@@ -8,77 +11,52 @@ class Category extends BaseModel
 {
     use CachesData, NodeTrait;
 
-	/**
-	 * The table associated with the model.
-	 *
-	 * @var string
-	 */
     protected $table = 'forum_categories';
 
-	/**
-	 * Indicates if the model should be timestamped.
-	 *
-	 * @var bool
-	 */
     public $timestamps = false;
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
     protected $fillable = ['title', 'description', 'accepts_threads', 'is_private', 'color', 'thread_count', 'post_count'];
 
-    /**
-     * Create a new category model instance.
-     *
-     * @param  array  $attributes
-     */
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
         $this->perPage = config('forum.preferences.pagination.categories');
     }
 
-    /**
-     * Relationship: Threads.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function threads()
+    public function threads(): HasMany
     {
         $withTrashed = Gate::allows('viewTrashedThreads');
         $query = $this->hasMany(Thread::class);
         return $withTrashed ? $query->withTrashed() : $query;
     }
 
-    /**
-     * Attribute: Paginated threads.
-     *
-     * @return \Illuminate\Pagination\LengthAwarePaginator
-     */
-    public function getThreadsPaginatedAttribute()
+    public function scopeTopLevel(Builder $query): Builder
+    {
+        return $query->where('parent_id', 0);
+    }
+
+    public function scopeAcceptsThreads(Builder $query): Builder
+    {
+        return $query->where('accepts_threads', 1);
+    }
+
+    public function scopeIsPrivate(Builder $query): Builder
+    {
+        return $query->where('is_private', 1);
+    }
+
+    public function getThreadsPaginatedAttribute(): LengthAwarePaginator
     {
         return $this->threads()->orderBy('pinned', 'desc')->orderBy('updated_at', 'desc')
             ->paginate(config('forum.general.pagination.threads'));
     }
 
-    /**
-     * Attribute: Newest thread.
-     *
-     * @return Thread
-     */
-    public function getNewestThreadAttribute()
+    public function getNewestThreadAttribute(): Thread
     {
         return $this->threads()->orderBy('created_at', 'desc')->first();
     }
 
-    /**
-     * Attribute: Latest active thread.
-     *
-     * @return Thread
-     */
-    public function getLatestActiveThreadAttribute()
+    public function getLatestActiveThreadAttribute(): Thread
     {
         return $this->threads()->orderBy('updated_at', 'desc')->first();
     }
