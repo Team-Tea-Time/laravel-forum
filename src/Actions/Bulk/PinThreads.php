dc949166c88589047c259919b2527c2b298ffaa8
Riari <3583774+Riari@users.noreply.github.com>
2020-11-21 19:52:58 +0000

    Fix LockThreads and PinThreads actions

--- src/Actions/Bulk/PinThreads.php
+++ src/Actions/Bulk/PinThreads.php
@@ -28,8 +28,10 @@ class PinThreads extends BaseAction
             $query = $query->whereNull(Thread::DELETED_AT);
         }
 
+        $threads = $query->get();
+
         // Return early if there are no eligible threads in the selection
-        if ($query->count() == 0) return null;
+        if ($threads->count() == 0) return null;
 
         $query->update(['pinned' => true]);
 
