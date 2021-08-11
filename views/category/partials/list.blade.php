b0b4b923877ac8789d55b7dbae8678154ca1ba4d
Riari <3583774+Riari@users.noreply.github.com>
2021-08-11 20:38:19 +0100

    Update category.partials.list
    Makes the "Re: [thread]" link only display if there's more than one post
    in the thread.
    
    Also center-aligns the contents of the subcategory rows to match the top
    level category rows.

--- views/category/partials/list.blade.php
+++ views/category/partials/list.blade.php
@@ -26,7 +26,7 @@
                             @include ('forum::partials.timestamp', ['carbon' => $category->newestThread->created_at])
                         </div>
                     @endif
-                    @if ($category->latestActiveThread)
+                    @if ($category->latestActiveThread && $category->latestActiveThread->post_count > 1)
                         <div>
                             <a href="{{ Forum::route('thread.show', $category->latestActiveThread->lastPost) }}">Re: {{ $category->latestActiveThread->title }}</a>
                             @include ('forum::partials.timestamp', ['carbon' => $category->latestActiveThread->lastPost->created_at])
@@ -41,7 +41,7 @@
         <div class="subcategories">
             @foreach ($category->children as $subcategory)
                 <div class="list-group-item">
-                    <div class="row">
+                    <div class="row align-items-center text-center">
                         <div class="col-sm text-md-start">
                             <a href="{{ Forum::route('category.show', $subcategory) }}" style="color: {{ $subcategory->color }};">{{ $subcategory->title }}</a>
                             <div class="text-muted">{{ $subcategory->description }}</div>
@@ -59,10 +59,10 @@
                             @if ($subcategory->newestThread)
                                 <div>
                                     <a href="{{ Forum::route('thread.show', $subcategory->newestThread) }}">{{ $subcategory->newestThread->title }}</a>
-                                    @include ('forum::partials.timestamp', ['carbon' => $subcategory->newestThread->lastPost->created_at])
+                                    @include ('forum::partials.timestamp', ['carbon' => $subcategory->newestThread->created_at])
                                 </div>
                             @endif
-                            @if ($subcategory->latestActiveThread)
+                            @if ($subcategory->latestActiveThread && $subcategory->latestActiveThread->post_count > 1)
                                 <div>
                                     <a href="{{ Forum::route('thread.show', $subcategory->latestActiveThread->lastPost) }}">Re: {{ $subcategory->latestActiveThread->title }}</a>
                                     @include ('forum::partials.timestamp', ['carbon' => $subcategory->latestActiveThread->lastPost->created_at])
