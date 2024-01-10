6e1ea46b208810ca3dda026fbc88c06b0ab7f2a7
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-01-10 23:43:09 +0000

    Override user model in FeatureTestCase setup before migrations are loaded

--- src/Tests/FeatureTestCase.php
+++ src/Tests/FeatureTestCase.php
@@ -9,14 +9,14 @@ class FeatureTestCase extends TestCase
 {
     protected function setUp(): void
     {
+        // Override user model in config
+        config(['forum.integration.user_model' => User::class]);
+
         parent::setUp();
 
         // Create dummy login route for the default redirection
         Route::get('login', ['as' => 'login'], function () {
             return '';
         });
-
-        // Override user model in config
-        config(['forum.integration.user_model' => User::class]);
     }
 }
