7aecf7bed9a8ec7ce624f475b888c26048f0dfbb
Rick Mann <3583774+Riari@users.noreply.github.com>
2026-05-08 21:30:05 +0100

    Fix Carbon usage

--- src/Frontend/Presets/LivewireTailwind/Components/Timestamp.php
+++ src/Frontend/Presets/LivewireTailwind/Components/Timestamp.php
@@ -2,14 +2,14 @@
 
 namespace TeamTeaTime\Forum\Frontend\Presets\LivewireTailwind\Components;
 
-use Carbon\Carbon;
+use Carbon\CarbonInterface;
 use Illuminate\Support\Facades\View as ViewFactory;
 use Illuminate\View\View;
 use Livewire\Component;
 
 class Timestamp extends Component
 {
-    public Carbon $carbon;
+    public CarbonInterface $carbon;
 
     public function render(): View
     {
